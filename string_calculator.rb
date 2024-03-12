bac828b4c62057c1e0ff8e77910d596e2b33cfa5
Kishore Rachakonda <kishore_personal1@yahoo.com>
2024-03-12 13:08:31 +0530

    Update string_calculator.rb
    2.Handle amount of numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -7,3 +7,10 @@ end
 puts add("")
 puts add("1")
 puts add("1,5")
+
+#Step2: Handle Amount of numbers
+def add(nums)
+	return 0 if nums.empty?
+	nums.split(",").map(&:to_i).sum
+end
+puts add("1,5,6,7")
